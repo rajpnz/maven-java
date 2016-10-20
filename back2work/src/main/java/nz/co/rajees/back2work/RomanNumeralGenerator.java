780b2e134b6fc89316b96d864e16c46d502c4836
rajpnz <rajpnz@gmail.com>
2016-10-21 11:19:40 +1300

    Refactor roman numeral generator

--- back2work/src/main/java/nz/co/rajees/back2work/RomanNumeralGenerator.java
+++ back2work/src/main/java/nz/co/rajees/back2work/RomanNumeralGenerator.java
@@ -4,76 +4,89 @@ public class RomanNumeralGenerator {
 
 	/**
 	 * Build the roman numeral representation of the given int
-	 * @param number Build the roman numeral representation for this int
-	 * @return a String that is a roman numeral 
-	 * @see <a href="http://codingdojo.org/cgi-bin/index.pl?KataRomanNumerals">Roman Numeral Kata</a>
+	 * 
+	 * @param number
+	 *            Build the roman numeral representation for this int
+	 * @return a String that is a roman numeral
+	 * @see <a href=
+	 *      "http://codingdojo.org/cgi-bin/index.pl?KataRomanNumerals">Roman
+	 *      Numeral Kata</a>
 	 */
-	public static String generateRomanNumeral(int number){
+	public static String generateRomanNumeral(int number) {
 		String romanNumeral = "";
 		int balance = number;
 		int numberOfNumerals = 0;
-		if((numberOfNumerals = Math.floorDiv(number, 1000)) > 0){
+		if ((numberOfNumerals = Math.floorDiv(number, 1000)) > 0) {
 			romanNumeral += buildNumeral("M", numberOfNumerals);
-			balance = number%1000;
+			balance = number % 1000;
 		}
-		if((numberOfNumerals = Math.floorDiv(balance, 100)) > 0){
-			if(balance >= 900 && balance <= 999){
+		if ((numberOfNumerals = Math.floorDiv(balance, 100)) > 0) {
+			if (balance >= 900 && balance <= 999) {
 				romanNumeral += "CM";
-			} else if(balance >= 400 && balance <= 499){
+			} else if (balance >= 400 && balance <= 499) {
 				romanNumeral += "CD";
-			} else{
+			} else {
 				romanNumeral += buildNumeral("C", numberOfNumerals);
 			}
-			
-			balance = balance%100;
+
+			balance = balance % 100;
 		}
-		if((numberOfNumerals = Math.floorDiv(balance, 50)) > 0){
-			//if we got in here then the balance is between 50 and 99
+		if ((numberOfNumerals = Math.floorDiv(balance, 50)) > 0) {
+			// if we got in here then the balance is between 50 and 99
 			romanNumeral += "L";
-			balance = balance%50;
+			balance = balance % 50;
 		}
-		if((numberOfNumerals = Math.floorDiv(balance, 40)) > 0){
-			//if we got in here then the balance is between 40 and 49
+		if ((numberOfNumerals = Math.floorDiv(balance, 40)) > 0) {
+			// if we got in here then the balance is between 40 and 49
 			romanNumeral += "XL";
-			balance = balance%40;
+			balance = balance % 40;
 		}
-		if((numberOfNumerals = Math.floorDiv(balance, 10)) > 0){
-			//if we got in here then the balance is between 0 and 39
+		if ((numberOfNumerals = Math.floorDiv(balance, 10)) > 0) {
+			// if we got in here then the balance is between 0 and 39
 			romanNumeral += buildNumeral("X", numberOfNumerals);
-			balance = balance%10;
+			balance = balance % 10;
 		}
-		//when we get here balance should be less than 10
-		switch(balance){
-			case 0:
-				break;
-			case 1:
-				romanNumeral+="I";
-				break;
-			case 2:
-				romanNumeral+="II";
-				break;
-			case 3:
-				romanNumeral+="III";
-				break;
-			case 4:
-				romanNumeral+="IV";
-				break;	
-			case 5:
-				romanNumeral+="V";
-				break;
-			case 6:
-				romanNumeral+="VI";
-				break;		
-			case 7:
-				romanNumeral+="VII";
-				break;	
-			case 8:
-				romanNumeral+="VIII";
-				break;
-			case 9:
-				romanNumeral+="IX";
-				break;					
-				
+		// when we get here balance should be less than 10
+		if(balance < 10){
+			romanNumeral += buildNumeralForNumberUnder10(balance);
+		}
+		return romanNumeral;
+	}
+
+	private static String buildNumeralForNumberUnder10(int numberUnder10) {
+
+		String romanNumeral = "";
+		switch (numberUnder10) {
+		case 0:
+			break;
+		case 1:
+			romanNumeral = "I";
+			break;
+		case 2:
+			romanNumeral = "II";
+			break;
+		case 3:
+			romanNumeral = "III";
+			break;
+		case 4:
+			romanNumeral = "IV";
+			break;
+		case 5:
+			romanNumeral = "V";
+			break;
+		case 6:
+			romanNumeral = "VI";
+			break;
+		case 7:
+			romanNumeral = "VII";
+			break;
+		case 8:
+			romanNumeral = "VIII";
+			break;
+		case 9:
+			romanNumeral = "IX";
+			break;
+
 		}
 		return romanNumeral;
 	}
@@ -82,7 +95,7 @@ public class RomanNumeralGenerator {
 		String numeral = "";
 		for (int i = 0; i < numberOfNumerals; i++) {
 			numeral += numeralValue;
-			
+
 		}
 		return numeral;
 	}
--- back2work/src/test/java/nz/co/rajees/back2work/RomanNumeralGeneratorTest.java
+++ back2work/src/test/java/nz/co/rajees/back2work/RomanNumeralGeneratorTest.java
@@ -8,6 +8,7 @@ public class RomanNumeralGeneratorTest {
 
 	@Test
 	public void testGenerateRomanNumeral() {
+		assertEquals("VII", RomanNumeralGenerator.generateRomanNumeral(7));
 		assertEquals("XIV", RomanNumeralGenerator.generateRomanNumeral(14));
 		assertEquals("LXXXIII", RomanNumeralGenerator.generateRomanNumeral(83));
 		assertEquals("CLXXIX", RomanNumeralGenerator.generateRomanNumeral(179));
