baeb7ca61248aa75bec60dbae914cbd4e64742aa
rajpnz <rajpnz@gmail.com>
2017-03-27 21:06:48 +1300

    Parsing some account stuff

--- back2work/src/main/java/nz/co/rajees/back2work/bank/AccountFactory.java
+++ back2work/src/main/java/nz/co/rajees/back2work/bank/AccountFactory.java
@@ -7,11 +7,13 @@ import java.util.regex.Pattern;
  */
 public class AccountFactory {
 
-    private static final Pattern ACCOUNT_ID_PATTERN = Pattern.compile("CRED-\\d{2}-\\d{3}-\\d{6}-\\d{3}");
+    private static final Pattern ACCOUNT_ID_PATTERN = Pattern.compile("(CRED)-(\\d{2}-\\d{3}-\\d{6}-\\d{3})");
 
     public AccountId createAccountId(String accountIdentifier){
         Matcher matcher = ACCOUNT_ID_PATTERN.matcher(accountIdentifier);
         if(matcher.matches()){
+            String type = matcher.group(1);
+            String accountNum = matcher.group(2);
             return new AccountId(accountIdentifier);
         } else {
             throw new IllegalArgumentException("Can't build account id from " + accountIdentifier);
