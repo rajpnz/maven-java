dcf11a46e56fb7846ed9fc937406ab8156f8318c
rajpnz <rajpnz@gmail.com>
2017-05-28 15:31:52 +1200

    Use a more elaborate json document and place in test resources

--- back2work/src/test/java/nz/co/rajees/back2work/JsonPathTest.java
+++ back2work/src/test/java/nz/co/rajees/back2work/JsonPathTest.java
@@ -6,10 +6,19 @@ import com.jayway.jsonpath.JsonPath;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
+/**
+ * @see <a href="https://github.com/json-path/JsonPath">JsonPath</a>
+ */
 public class JsonPathTest {
 
     private static final String TEST_JSON = " " +
@@ -23,9 +32,14 @@ public class JsonPathTest {
     private Object document;
 
     @Before
-    public void setUp(){
+    public void setUp() throws IOException {
         //document will only be read in once
-        document = Configuration.defaultConfiguration().jsonProvider().parse(TEST_JSON);
+        String pathToFile = "src/test/resources/test.json";
+        File file = new File(pathToFile);
+        try (FileInputStream fis = new FileInputStream(file)) {
+            document = Configuration.defaultConfiguration().jsonProvider().parse(fis,"cp1252");
+        }
+
     }
 
     @Test
@@ -38,9 +52,20 @@ public class JsonPathTest {
     @Test
     public void testPartTwoAsExpected(){
         //read from document
-        Integer actual = JsonPath.read(document, "$.someThing.innerThing.partTwo");
+        Integer actual = JsonPath.read(TEST_JSON, "$.someThing.innerThing.partTwo");
         assertThat(actual, is(equalTo(123)));
     }
 
+    @Test
+    public void testAuthor(){
+        String author0 = JsonPath.read(document, "$.store.book[0].author");
+        assertThat(author0, is(equalTo("Nigel Rees")));
+    }
 
+    @Test
+    public void testAllAuthors(){
+        List<String> authors = JsonPath.read(document, "$.store.book[*].author");
+        assertThat(authors, containsInAnyOrder("Nigel Rees", "Evelyn Waugh","Herman Melville",
+                "J. R. R. Tolkien"));
+    }
 }
