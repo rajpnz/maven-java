74ac587e2558734839ad80847c654c961e2c6109
Rajees Patel <rajpnz@gmail.com>
2017-07-16 20:06:59 +1200

    Add script that runs a jar file

---
+++ back2work/scripts/runjar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script runs a fat jar that is located in the same directory as the script
+java -cp back2work-0.0.1-SNAPSHOT.jar nz.co.rajees.back2work.Example
+echo "=================================================="
+
